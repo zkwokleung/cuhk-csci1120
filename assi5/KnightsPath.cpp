1708f1aaf769e70976eadebaa7baa3e3af9f39b1
Andrew SZETO <zkwokleung@gmail.com>
2021-11-09 16:53:42 +0800

    UPDATE: Finished

--- assi5/KnightsPath.cpp
+++ assi5/KnightsPath.cpp
@@ -1,20 +1,24 @@
 #include <iostream>
+#include <cmath>
 #include "KnightsPath.h"
 
 using namespace std;
 
 KnightsPath::KnightsPath(int r, int c)
 {
+    // Init board
     for (int i = 0; i < N; i++)
         for (int j = 0; j < N; j++)
             board[i][j] = -1;
 
+    // Init private fields
     steps = 0;
-
     previousC = -1;
     previousR = -1;
     currentC = c;
     currentR = r;
+    // init position
+    board[r][c] = 0;
 }
 
 void KnightsPath::print() const
@@ -30,27 +34,38 @@ void KnightsPath::print() const
         // Col name
         cout << i;
 
+        // Content
         for (int j = 0; j < N; j++)
         {
-            cout << "  ";
             switch (board[i][j])
             {
             case -1:
-                cout << '.';
+                cout << "  .";
                 break;
 
             case 0:
-                cout << 'k';
+                cout << "  k";
+                break;
 
             default:
                 if (steps == board[i][j])
-                    cout << 'K';
+                    cout << "  K";
                 else
+                {
+                    if (board[i][j] < 10)
+                        cout << "  ";
+                    else
+                        cout << " ";
                     cout << board[i][j];
+                }
                 break;
             }
         }
+        cout << endl;
     }
+
+    // Steps
+    cout << "Steps: " << steps << endl;
 }
 
 int KnightsPath::getSteps() const
@@ -60,15 +75,70 @@ int KnightsPath::getSteps() const
 
 bool KnightsPath::isValid(int r, int c) const
 {
+    // Check if it is within range
+    // cout << "DEBUG: " << (c >= 0 && c < KnightsPath::N) << " " << (r >= 0 && r < KnightsPath::N) << " " << (board[r][c] == -1) << endl;
+    if ((c >= 0 && c < KnightsPath::N) &&
+        (r >= 0 && r < KnightsPath::N) &&
+        (board[r][c] == -1))
+    {
+        // Check if it is a valid move for a knight
+        // cout << "DEBUG: " << (-2 < 2) << endl;
+        for (int i = -2; i <= 2; i += 4)
+        {
+            for (int j = -1; j <= 1; j += 2)
+            {
+                // cout << "DEBUG: (" << (currentR + i == r) << " && " << (currentC + j == c) << ") || (" << (currentR + j == r) << " && " << (currentC + i == c) << ")" << endl;
+                if ((currentR + i == r && currentC + j == c) ||
+                    (currentR + j == r && currentC + i == c))
+                {
+                    // Check if it is going backward by its distance
+                    if (steps > 0)
+                        return pow(r - previousR, 2) + pow(c - previousC, 2) > 4;
+                    else
+                        return true;
+                }
+            }
+        }
+    }
+
     return false;
 }
 
 bool KnightsPath::hasMoreMoves() const
 {
+    // Check for every possible move if they are a valid move
+    for (int i = -2; i <= 2; i += 4)
+    {
+        for (int j = -1; j <= 1; j += 2)
+        {
+            if (isValid(currentR + i, currentC + j) ||
+                isValid(currentR + j, currentC + i))
+                return true;
+        }
+    }
     return false;
 }
 
 bool KnightsPath::move(int r, int c)
 {
-    return false;
+    if (isValid(r, c))
+    {
+        // Change the previous position
+        previousR = currentR;
+        previousC = currentC;
+
+        // Change the current position
+        currentC = c;
+        currentR = r;
+
+        // change position on board to the step number
+        board[r][c] = ++steps;
+
+        return true;
+    }
+    else
+    {
+        // Update nothing
+        return false;
+    }
 }
--- assi5/walk.cpp
+++ assi5/walk.cpp
@@ -3,9 +3,72 @@
 
 using namespace std;
 
+void get_statring_input(int *col, int *row)
+{
+    char tmp_col;
+    int tmp_row;
+    cout << "Enter starting position (col row): ";
+    cin >> tmp_col >> tmp_row;
+
+    // Check if the input is within range and if it is a valid move
+    if (tmp_col - 'A' < 0 || tmp_col - 'A' > KnightsPath::N ||
+        tmp_row < 0 || tmp_row > KnightsPath::N)
+    {
+        // Recursively call until received a valid input
+        cout << "Invalid. Try again!" << endl;
+        get_statring_input(col, row);
+    }
+    else
+    {
+        *col = tmp_col - 'A';
+        *row = tmp_row;
+    }
+}
+
+void get_move_input(KnightsPath kp, int *row, int *col)
+{
+    char tmp_col;
+    int tmp_row;
+    cout << "Move the knight (col row): ";
+    cin >> tmp_col >> tmp_row;
+
+    // Check if the input is within range and if it is a valid move
+    if (kp.isValid(tmp_row, tmp_col - 'A'))
+    {
+        *col = tmp_col - 'A';
+        *row = tmp_row;
+    }
+    else
+    {
+        // Recursively call until received a valid input
+        cout << "Invalid. Try again!" << endl;
+        get_statring_input(col, row);
+    }
+}
+
 int main(void)
 {
-    KnightsPath kp = KnightsPath(0, 0);
-    kp.print();
+    int r = 0, c = 0;
+
+    // Starting position
+    get_statring_input(&c, &r);
+    // Initialize
+    KnightsPath kp = KnightsPath(r, c);
+
+    // Game loop
+    do
+    {
+        kp.print();
+        get_move_input(kp, &r, &c);
+        kp.move(r, c);
+    } while (kp.hasMoreMoves());
+
+    // End game message
+    cout << "Finished! No more moves!" << endl;
+    if (kp.getSteps() > (KnightsPath::N * KnightsPath::N / 2))
+        cout << "Well done!";
+    else
+        cout << "Still drunk? Walk wiser!";
+
     return 0;
 }
