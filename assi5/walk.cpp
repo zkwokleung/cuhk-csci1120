e23a56248b287329c0c2cf0f3974562bba6f1990
Andrew SZETO <zkwokleung@gmail.com>
2021-11-09 18:50:48 +0800

    FIX: bug where used the old r and c to move

--- assi5/walk.cpp
+++ assi5/walk.cpp
@@ -29,30 +29,21 @@ void get_statring_input(int *col, int *row)
     }
 }
 
-void get_move_input(KnightsPath kp, int *row, int *col)
+// Get input without any checking
+void get_move_input(int *row, int *col)
 {
     char tmp_col;
     int tmp_row;
     cout << "Move the knight (col row): ";
     cin >> tmp_col >> tmp_row;
-
-    // Check if the input is within range and if it is a valid move
-    if (kp.isValid(tmp_row, tmp_col - 'A'))
-    {
-        *col = tmp_col - 'A';
-        *row = tmp_row;
-    }
-    else
-    {
-        // Recursively call until received a valid input
-        cout << "Invalid move. Try again!" << endl;
-        get_move_input(kp, col, row);
-    }
+    *col = tmp_col - 'A';
+    *row = tmp_row;
 }
 
 int main(void)
 {
     int r = 0, c = 0;
+    bool flag = false; // A continue flag for input reading part.
 
     // Starting position
     get_statring_input(&c, &r);
@@ -63,14 +54,22 @@ int main(void)
     do
     {
         kp.print();
-        get_move_input(kp, &r, &c);
-        kp.move(r, c);
+        do
+        {
+            flag = false;
+            get_move_input(&r, &c);
+            if (!kp.move(r, c))
+            {
+                cout << "Invalid move. Try again!" << endl;
+                flag = true;
+            }
+        } while (flag);
     } while (kp.hasMoreMoves());
 
     // End game message
     kp.print();
     cout << "Finished! No more moves!" << endl;
-    if (kp.getSteps() > (KnightsPath::N * KnightsPath::N / 2))
+    if (kp.getSteps() >= (KnightsPath::N * KnightsPath::N / 2))
         cout << "Well done!";
     else
         cout << "Still drunk? Walk wiser!";
